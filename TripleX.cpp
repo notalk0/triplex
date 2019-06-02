2be986c75306d4a1e514947164e0a8231c00698e
Reina Liew <notalk_0@hotmail.com>
2019-06-02 22:04:24 +0800

    Initial Commit

---
+++ TripleX.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to continue...";
+
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    // Print CodeSum and CodeProduct to the terminal
+    std::cout << std::endl;
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiple to give: " << CodeProduct << std::endl;
+
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    return 0;
+}
