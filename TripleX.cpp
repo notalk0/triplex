de00e29b8a7792c88cc9145cba8defac4a7c0d5d
Reina Liew <notalk_0@hotmail.com>
2019-06-03 10:45:01 +0800

    Improve

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,31 +1,68 @@
 #include <iostream>
+#include <ctime>
 
-int main()
+void PrintIntroduction(int Difficulty)
 {
-    std::cout << "You are a secret agent breaking into a secure server room";
-    std::cout << std::endl;
-    std::cout << "You need to enter the correct codes to continue...";
+    std::cout << "You are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure server room\nEnter the correct codes to continue...\n";
+}
 
-    const int CodeA = 4;
-    const int CodeB = 3;
-    const int CodeC = 2;
+bool PlayGame(int Difficulty)
+{
+    PrintIntroduction(Difficulty);
+
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
 
     // Print CodeSum and CodeProduct to the terminal
     std::cout << std::endl;
-    std::cout << "+ There are 3 numbers in the code" << std::endl;
-    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
-    std::cout << "+ The codes multiple to give: " << CodeProduct << std::endl;
+    std::cout << "+ There are 3 numbers in the code \n";
+    std::cout << "+ The codes add-up to: " << CodeSum;
+    std::cout << "\n+ The codes multiple to give: " << CodeProduct << std::endl;
 
+    // Store player guess
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // Check if the player guess is correct
+    if (CodeSum == GuessSum && CodeProduct == GuessProduct)
+    {
+        std::cout << "\n*** Well done agent! You have extracted a file! Keep going! ***\n\n";
+        return true;
+    }
+    else
+    {
+        std::cout << "\n*** You entered the wrong code! Careful agent! Try again! ***\n\n";
+        return false;
+    }
+}
+
+int main()
+{
+
+    srand(time(NULL)); // Create new random sequence based on time of day
+
+    int LevelDifficulty = 1;
+    const int MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) // Loop game until all levels completed
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();  // Clear any errors
+        std::cin.ignore(); // Discards the buffer
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+    }
+    std::cout << "\n*** Greate work agent! You got all the files! Now get out of there! ***\n";
     return 0;
 }
